3cbd9322c42145f733b00d8762a821bd2367450d
carlosjtandem <soycarlosjavier14@gmail.com>
2024-02-16 15:26:36 -0500

    Added unit of work and domain events

--- src/CleanArchitecture/CleanArchitecture.Infraestructure/ApplicationDbContext.cs
+++ src/CleanArchitecture/CleanArchitecture.Infraestructure/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
 using CleanArchitecture.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleaArchitecture.Infraestructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options) : base(options)
+    private readonly IPublisher _publisher;
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
     {
-
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -15,5 +17,33 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         // para que se apliquen las configuracion del model builder
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);  //el assembly yiene el DbContext, esto significa que cuando el modelo este configurado este va a escanear este asembly encontrando cada configuracion de las entidades.. eso lo lee del directorio Configurations.
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+        .Entries<Entity>()
+        .Select(entry => entry.Entity)
+        .SelectMany(entity =>
+        {
+            var domainEvents = entity.GetDomainsEvents();
+            entity.ClearDomainEvents();
+            return domainEvents;
+        }).ToList();
+
+        foreach (var damainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvents);
+        }
     }
 }
